688e25e18180a438916793599e64b30dc5f14ce4
Gustaf Mossakowski <gustaf@koenige.org>
2021-02-15 00:48:23 +0100

    PGN: hide eval comments

--- tournaments/pgn.inc.php
+++ tournaments/pgn.inc.php
@@ -233,6 +233,7 @@ function pgn_to_html($pgn, $extra_comment = []) {
 	}
 	$moves = $pgn['moves'];
 	$moves = preg_replace('/{\[\%emt \d+:\d+:\d+\]} /', '', $moves);
+	$moves = preg_replace('/\[\%eval [-\d#.]*\]/', '', $moves);
 	$moves = str_replace('Diagramm #', '', $moves);
 	$moves = str_replace('{', ' { ', $moves);
 	$moves = str_replace('}', ' } ', $moves);
