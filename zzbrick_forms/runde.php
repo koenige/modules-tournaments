bdd5247b88518f70901908930ca93b3cdce1dd67
Gustaf Mossakowski <gustaf@koenige.org>
2021-03-21 16:20:13 +0100

    events: change field numbering of events table definition

--- zzbrick_forms/runde.php
+++ zzbrick_forms/runde.php
@@ -2,7 +2,7 @@
 
 // Zugzwang Project
 // deutsche-schachjugend.de
-// Copyright (c) 2014-2017, 2019-2020 Gustaf Mossakowski <gustaf@koenige.org>
+// Copyright (c) 2014-2017, 2019-2021 Gustaf Mossakowski <gustaf@koenige.org>
 // Skript: Einzelner Termin bearbeiten
 
 
@@ -15,42 +15,43 @@ $zz['title'] = 'Runden';
 $zz['where']['main_event_id'] = $event['event_id'];
 $zz['where']['event_category_id'] = wrap_category_id('zeitplan/runde');
 
-$zz['fields'][6]['default'] = $event['zeitplan_max'];
+$zz['fields'][4]['default'] = $event['zeitplan_max'];
 
-$zz['fields'][3]['fields'] = [
+$zz['fields'][2]['fields'] = [
 	'main_event_id[identifier]', 'runde_no', 'identifier'
 ];
-$zz['fields'][3]['conf_identifier']['concat'] = [
+$zz['fields'][2]['conf_identifier']['concat'] = [
 	'/', '/runde/'
 ];
 
-$zz['fields'][19]['hide_in_list'] = true;
-$zz['fields'][19]['hide_in_form'] = true;
+$zz['fields'][26]['hide_in_list'] = true;
+$zz['fields'][26]['hide_in_form'] = true;
 
 $zz['fields'][35]['type'] = 'hidden';
 $zz['fields'][35]['hide_in_form'] = true;
 $zz['fields'][35]['value'] = $event['website_id'];
 
-unset($zz['fields'][5]); // Reihe
+unset($zz['fields'][15]); // Reihe
 unset($zz['fields'][24]); // Termine/Kategorien
 unset($zz['fields'][31]); // Termine/Websites
 unset($zz['fields'][12]); // Ort
 unset($zz['fields'][13]); // Ort, frei
-unset($zz['fields'][37]); // Anreisser
-unset($zz['fields'][32]); // Beschreibung
+unset($zz['fields'][8]); // Anreisser
+unset($zz['fields'][14]); // Beschreibung
 unset($zz['fields'][33]); // Anmerkungen
 unset($zz['fields'][34]); // Offen?
 unset($zz['fields'][10]); // Termine/Organisationen
-unset($zz['fields'][14]); // Termine/Links
-unset($zz['fields'][15]); // Termine/Teilnehmerzahlen
-unset($zz['fields'][16]); // Teilnahmen
-unset($zz['fields'][18]); // Termine/Bankkonten
-unset($zz['fields'][21]); // Anmeldeinfo
+unset($zz['fields'][64]); // Termine/Links
+unset($zz['fields'][65]); // Termine/Teilnehmerzahlen
+unset($zz['fields'][61]); // Teilnahmen
+unset($zz['fields'][68]); // Termine/Bankkonten
+unset($zz['fields'][17]); // Anmeldeinfo
 
-unset($zz['fields'][38]); // direct link
+unset($zz['fields'][16]); // direct link
 unset($zz['fields'][39]); // Berechtigung
 unset($zz['fields'][40]); // Berechtigung
 unset($zz['fields'][41]); // Berechtigung
+unset($zz['fields'][56]); // Veranstaltungsjahr
 
 // Bedingung für Runde immer wahr
 $zz['conditions'][1]['where'] = '';
