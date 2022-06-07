5b24ce49dea719e8727f0cd593c72f9256c4907a
Gustaf Mossakowski <gustaf@koenige.org>
2022-06-07 11:22:08 +0200

    player messages: allow to restrict form to series event_id, adapt filters

--- zzbrick_forms/playermessages.php
+++ zzbrick_forms/playermessages.php
@@ -15,6 +15,13 @@
 
 $zz = zzform_include_table('spielernachrichten');
 
+$event_ids = [];
+if (!empty($brick['data']['event_id'])) {
+	$event_ids = mf_tournaments_series_events($brick['data']['event_id']);
+	$zz['sql'] = wrap_edit_sql($zz['sql'], 'WHERE', sprintf('event_id IN (%s)', implode(',', $event_ids)));
+	$zz['title'] .= ': <br>'.$brick['data']['series_short'].' '.$brick['data']['year'];
+}
+
 $data = [];
 $data['current_time'] = date("Y-m-d H:i:s", time());
 
@@ -35,9 +42,13 @@ if (!empty($_POST['sent_date'])) {
 
 $zz['explanation'] = wrap_template('playermessage-form', $data);
 
-$zz['filter'][1]['sql'] = 'SELECT DISTINCT(processed), processed
-	FROM /*_PREFIX_*/spieler_nachrichten
-	ORDER BY processed DESC';
+$zz['filter'][1]['sql'] = sprintf('SELECT DISTINCT(processed), processed
+	FROM spieler_nachrichten
+	LEFT JOIN participations
+		ON spieler_nachrichten.teilnehmer_id = participations.participation_id
+	%s
+	ORDER BY processed DESC'
+	, $event_ids ? sprintf(' WHERE event_id IN (%s)', implode(',', $event_ids)) : '');
 $zz['filter'][1]['title'] = 'Verarbeitet';
 $zz['filter'][1]['identifier'] = 'processed';
 $zz['filter'][1]['type'] = 'list';
@@ -45,13 +56,15 @@ $zz['filter'][1]['field_name'] = 'processed';
 $zz['filter'][1]['where'] = '/*_PREFIX_*/spieler_nachrichten.processed';
 $zz['filter'][1]['default_selection'] = 'NULL';
 
-$zz['filter'][2]['sql'] = 'SELECT contact_id, contact_short
+$zz['filter'][2]['sql'] = sprintf('SELECT contact_id, contact_short
 	FROM spieler_nachrichten
 	LEFT JOIN participations
 		ON participations.participation_id = spieler_nachrichten.teilnehmer_id
 	LEFT JOIN contacts
 		ON participations.federation_contact_id = contacts.contact_id
-	ORDER BY contact_short';
+	%s
+	ORDER BY contact_short'
+	, $event_ids ? sprintf(' WHERE event_id IN (%s)', implode(',', $event_ids)) : '');
 $zz['filter'][2]['title'] = 'Verband';
 $zz['filter'][2]['identifier'] = 'federation';
 $zz['filter'][2]['type'] = 'list';
