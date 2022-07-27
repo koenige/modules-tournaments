84cc47825d9cc71b6b6b601de717338a3acf4d56
Gustaf Mossakowski <gustaf@koenige.org>
2022-07-27 08:45:32 +0200

    team placeholder: include function my_team_form() directly in placeholder function, no need for separate function

--- zzbrick_placeholder/team.inc.php
+++ zzbrick_placeholder/team.inc.php
@@ -20,10 +20,53 @@ function mod_tournaments_placeholder_team($brick) {
 	} else {
 		list($year, $identifier, $team_idf) = [$brick['vars'][1], $brick['vars'][2]];
 	}
-	require_once $zz_setting['inc'].'/custom/zzwrap/team.inc.php';
-	$status = !empty($brick['local_settings']['status']) ? $brick['local_settings']['status'] : ['offen', 'teiloffen'];
-	$team = my_team_form([$year, $identifier, $team_idf], $status);
+
+	$sql = 'SELECT team_id, team, team_no, meldung
+			, event_id, event, IFNULL(event_year, YEAR(date_begin)) AS year
+			, IFNULL(place, places.contact) AS turnierort
+			, CONCAT(date_begin, IFNULL(CONCAT("/", date_end), "")) AS duration
+			, DATEDIFF(date_end, date_begin) AS dauer_tage
+			, events.identifier AS event_identifier
+			, series_category_id
+			, main_series.category_short AS main_series
+			, IF(LENGTH(main_series.path) > 7, SUBSTRING_INDEX(main_series.path, "/", -1), NULL) AS main_series_path
+			, SUBSTRING_INDEX(turnierformen.path, "/", -1) AS turnierform
+			, club_contact_id AS contact_id, clubs.contact
+			, IF(tournaments.zimmerbuchung = "ja", 1, NULL) AS zimmerbuchung
+			, IF(tournaments.gastspieler = "ja", 1, NULL) AS gastspieler
+			, CONCAT("event_id:", events.event_id) AS event_rights
+			, place_categories.parameters
+		FROM teams
+		LEFT JOIN events USING (event_id)
+		LEFT JOIN tournaments USING (event_id)
+		LEFT JOIN contacts places
+			ON places.contact_id = events.place_contact_id
+		LEFT JOIN addresses USING (contact_id)
+		LEFT JOIN contacts clubs
+			ON clubs.contact_id = teams.club_contact_id
+		LEFT JOIN categories series
+			ON events.series_category_id = series.category_id
+		LEFT JOIN categories main_series
+			ON series.main_category_id = main_series.category_id
+		LEFT JOIN categories turnierformen
+			ON tournaments.turnierform_category_id = turnierformen.category_id
+		LEFT JOIN categories place_categories
+			ON places.contact_category_id = place_categories.category_id
+		WHERE teams.identifier = "%d/%s/%s"';
+	$sql = sprintf($sql, $year, wrap_db_escape($identifier), wrap_db_escape($team_idf));
+	$team = wrap_db_fetch($sql);
 	if (!$team) wrap_quit(404);
+
+	$status = !empty($brick['local_settings']['status']) ? $brick['local_settings']['status'] : ['offen', 'teiloffen'];
+	if (!in_array($team['meldung'], $status)) wrap_quit(403);
+
+	require_once $zz_setting['inc'].'/custom/zzwrap/team.inc.php';
+	if (!my_team_access($team['team_id'], ['Teilnehmer'])) wrap_quit(403);
+
+	if ($team['parameters']) {
+		parse_str($team['parameters'], $parameters);
+		$team += $parameters;
+	}
 
 	if (!empty($brick['local_settings']['internal'])) {
 		$bc_template = '<a href="'.$zz_setting['events_internal_path'].'/%s/">%s</a>';
