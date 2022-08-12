08eb918dc0a50dcd4d8043159d4a9ccab3306543
Gustaf Mossakowski <gustaf@koenige.org>
2022-08-12 12:53:48 +0200

    access: expand own team to usergroup `betreuer`, if team application is complete and team status is participant

--- zzbrick_rights/access.inc.php
+++ zzbrick_rights/access.inc.php
@@ -31,8 +31,8 @@ function mf_tournaments_team_access($team_id, $status = ['Teilnehmer', 'Teilnahm
 	$event_rights = wrap_db_fetch($sql, '', 'single value');
 	if (brick_access_rights(['Organisator', 'Technik'], $event_rights)) return true;
 
-	$eigene_teams = mf_tournaments_team_own($status);
-	if (!in_array($team_id, $eigene_teams)) return false;
+	$teams = mf_tournaments_team_own($status);
+	if (!in_array($team_id, $teams)) return false;
 	return true;
 }
 
@@ -43,22 +43,34 @@ function mf_tournaments_team_access($team_id, $status = ['Teilnehmer', 'Teilnahm
  * @return array
  */
 function mf_tournaments_team_own($status = ['Teilnehmer', 'Teilnahmeberechtigt']) {
-	if (empty($_SESSION['usergroup'][wrap_id('usergroups', 'team-organisator')])) {
+	if (empty($_SESSION['usergroup'][wrap_id('usergroups', 'team-organisator')])
+		AND empty($_SESSION['usergroup'][wrap_id('usergroups', 'betreuer')])
+	) {
 		return [];
 	}
 
-	$sql = 'SELECT team_id
+	$sql = 'SELECT participation_id, team_id, team_status, usergroup_id, meldung
 		FROM participations
 		LEFT JOIN teams USING (team_id)
-		WHERE usergroup_id = %d
+		WHERE usergroup_id IN (%d, %d)
 		AND person_id = %d
 		AND team_status IN ("%s")
 	';
 	$sql = sprintf($sql
 		, wrap_id('usergroups', 'team-organisator')
+		, wrap_id('usergroups', 'betreuer')
 		, $_SESSION['person_id']
 		, implode('","', $status)
 	);
-	$teams = wrap_db_fetch($sql, 'team_id', 'single value');
+	$participations = wrap_db_fetch($sql, 'participation_id');
+	$teams = [];
+	foreach ($participations as $participation) {
+		if ($participation['usergroup_id'] === wrap_id('usergroups', 'betreuer')) {
+			if ($participation['meldung'] !== 'komplett') continue;
+			if ($participation['team_status'] !== 'Teilnehmer') continue;
+		}
+		$teams[] = $participation['team_id'];
+	}
+	$teams = array_unique($teams);
 	return $teams;
 }
