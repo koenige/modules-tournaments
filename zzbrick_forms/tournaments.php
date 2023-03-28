e7db027b756019f66d2c55d43032f5a7d74c9ad3
Gustaf Mossakowski <gustaf@koenige.org>
2023-03-28 14:22:35 +0200

    tournaments form: remove unneeded query; bugfix if[insert] for adding old tournament data, not if[add]; translate text

--- zzbrick_forms/tournaments.php
+++ zzbrick_forms/tournaments.php
@@ -23,19 +23,18 @@ $sql = 'SELECT tournament_id
 	ORDER BY date_begin DESC
 	LIMIT 1';
 $sql = sprintf($sql, $brick['data']['series_category_id'], $brick['data']['event_id']);
-$data = wrap_db_fetch($sql, 'tournament_id');
 $brick['data']['last_tournament_id'] = wrap_db_fetch($sql, '', 'single value');
 
 if ($brick['data']['last_tournament_id']) {
-	$zz['if']['add']['explanation'] = sprintf(
-		'<ul><li>Statt Eingabe: <a href="./?add=%s">Übernahme der Daten vom letzten Turnier dieser Reihe</a></li></ul>',
+	$zz['if']['insert']['explanation'] = sprintf(
+		'<ul><li><a href="./?add=%s">'.wrap_text('Instead of input: Transfer of the settings from the last tournament in this series').'</a></li></ul>',
 		$brick['data']['last_tournament_id']
 	);
 }
 
 if (!empty($zz['fields'][25]))
-	$zz['fields'][25]['explanation'] = 'Hier bitte nur Gesamtdateien hochladen.
-	<br>Für Runden gibt es die Möglichkeit zum Upload bei der <a href="../runde/">Rundenübersicht</a>.';
+	$zz['fields'][25]['explanation'] = wrap_text('Please only upload files for the entire tournament here.')
+	.'<br>'.sprintf(wrap_text('For rounds, there is a possibility to upload a PGN file in the <a href="%s">round overview</a>.'), '../runde/');
 
 $zz['where']['event_id'] = $brick['data']['event_id'];
 $zz['access'] = 'add_then_edit';
