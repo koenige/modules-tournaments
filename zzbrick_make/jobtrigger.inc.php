74f3a878dac90e4dd88678018303b2ecda945fa0
Gustaf Mossakowski <gustaf@koenige.org>
2023-04-26 23:56:13 +0200

    jobtrigger: read job_url from _jobqueue, no need to create URL with runde_no, event_id

--- zzbrick_make/jobtrigger.inc.php
+++ zzbrick_make/jobtrigger.inc.php
@@ -2,7 +2,7 @@
 
 /**
  * tournaments module
- * trigger a cron job
+ * trigger a background job
  *
  * Part of »Zugzwang Project«
  * https://www.zugzwang.org/modules/tournaments
@@ -30,9 +30,9 @@ function mod_tournaments_make_jobtrigger($params) {
 	$sql = 'SELECT category_id, category
 			, SUBSTRING_INDEX(path, "/", -1) AS path
 			, parameters,
-			(SELECT COUNT(*) FROM _jobqueue laufend
-				WHERE categories.category_id = laufend.job_category_id
-				AND NOT ISNULL(laufend.started) AND ISNULL(laufend.finished)) AS laufend,
+			(SELECT COUNT(*) FROM _jobqueue running
+				WHERE categories.category_id = running.job_category_id
+				AND NOT ISNULL(running.started) AND ISNULL(running.finished)) AS running,
 			(SELECT COUNT(*) FROM _jobqueue todo
 				WHERE categories.category_id = todo.job_category_id
 				AND ISNULL(todo.started) AND ISNULL(todo.finished)) AS todo
@@ -47,61 +47,54 @@ function mod_tournaments_make_jobtrigger($params) {
 		parse_str($category['parameters'], $parameter);
 		if (empty($parameter['max_requests'])) $parameter['max_requests'] = 1;
 		if (empty($parameter['max_time'])) $parameter['max_time'] = 30;
-		$cronjob = [];
-		if ($category['laufend'] < $parameter['max_requests']) {
-			$sql = 'SELECT job_id, events.identifier AS event_identifier, _jobqueue.runde_no
+		$job = [];
+		if ($category['running'] < $parameter['max_requests']) {
+			$sql = 'SELECT job_id, job_url
 				FROM _jobqueue
-				LEFT JOIN events USING (event_id)
 				WHERE ISNULL(_jobqueue.started) AND ISNULL(_jobqueue.finished)
 				AND job_category_id = %d
 				ORDER BY priority
 				LIMIT 1';
 			$sql = sprintf($sql, $category['category_id']);
-			$cronjob = wrap_db_fetch($sql);
+			$job = wrap_db_fetch($sql);
 			$request = 1;
 		}
-		if (!$cronjob) {
+		if (!$job) {
 			// Laufen gerade Jobs im Rahmen der vorgegebenen Zeit?
-			$sql = 'SELECT job_id, events.identifier AS event_identifier, _jobqueue.runde_no
+			$sql = 'SELECT COUNT(*)
 				FROM _jobqueue
-				LEFT JOIN events USING (event_id)
 				WHERE job_category_id = %d
 				AND NOT ISNULL(_jobqueue.started) AND ISNULL(_jobqueue.finished)
 				AND DATE_ADD(started, INTERVAL %d SECOND) > NOW()
 			';
 			$sql = sprintf($sql, $category['category_id'], $parameter['max_time']);
-			$laufend = wrap_db_fetch($sql, 'job_id');
+			$running = wrap_db_fetch($sql, '', 'single value');
 
-			if (count($laufend) >= $parameter['max_requests']) continue;
+			if ($running >= $parameter['max_requests']) continue;
 			// abgestürzte Prozesse aus Job-Liste erneut aufrufen
-			$sql = 'SELECT job_id, events.identifier AS event_identifier, _jobqueue.runde_no
+			$sql = 'SELECT job_id, job_url
 				FROM _jobqueue
-				LEFT JOIN events USING (event_id)
 				WHERE job_category_id = %d
 				AND NOT ISNULL(_jobqueue.started) AND ISNULL(_jobqueue.finished)
 				AND DATE_ADD(started, INTERVAL %d SECOND) < NOW()
 			';
 			$sql = sprintf($sql, $category['category_id'], $parameter['max_time']);
-			$cronjobs = wrap_db_fetch($sql, 'job_id');
+			$jobs = wrap_db_fetch($sql, 'job_id');
 			// nur soviele Jobs wie max_requests
-			$cronjob = array_shift($cronjobs);
+			$job = array_shift($jobs);
 			// @todo nicht endlos probieren, sondern irgendwann auf inaktiv stellen!
-			$request = count($laufend) + 1;
+			$request = $running + 1;
 		}
-		if (!$cronjob) continue;
+		if (!$job) continue;
 		$realm = sprintf('%s-%d', $category['path'], $request);
 		$locked = wrap_lock($realm, 'sequential', $parameter['max_time']);
 		if ($locked) continue;
 		$sql = 'UPDATE _jobqueue SET started = NOW(), job_category_no = %d WHERE job_id = %d';
-		$sql = sprintf($sql, $request, $cronjob['job_id']);
+		$sql = sprintf($sql, $request, $job['job_id']);
 		$success = wrap_db_query($sql, E_USER_NOTICE);
 		if (!$success) continue;
 
-		$url = sprintf('/_jobs/%s/%s/%s',
-			$category['path'], $cronjob['event_identifier'],
-			$cronjob['runde_no'] ? $cronjob['runde_no'].'/' : ''
-		);
-		wrap_trigger_protected_url($url);
+		wrap_trigger_protected_url($job['job_url']);
 		sleep(5); // Warte etwas, bevor es weitergeht.
 	}
 
