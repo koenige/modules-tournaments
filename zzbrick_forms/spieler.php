1aeacd0390c5230d80dd75ed66a10f9fb51531d6
Gustaf Mossakowski <gustaf@koenige.org>
2024-05-06 23:25:38 +0200

    spieler: change field definitions based on field name; add access rights `tournaments_players_change`, `tournaments_players_edit` instead of hardcoded groups; always show nation, FIDE ID, federation

--- zzbrick_forms/spieler.php
+++ zzbrick_forms/spieler.php
@@ -33,62 +33,75 @@ foreach ($zz['fields'] as $no => $field) {
 	case 't_verein':
 		$zz['fields'][$no]['merge_ignore'] = true;
 		break;
+	
+	case 'contact_id':
+		$zz['fields'][$no]['display_field'] = 'person';
+		// if an import error occured
+		if (wrap_access('tournaments_players_change', $brick['data']['event_rights']))
+			$zz['fields'][$no]['type'] = 'select';
+		break;
+
+	case 'team_id':
+		$zz['fields'][$no]['display_field'] = 'team';
+		if (!empty($brick['data']['event_team'])) {
+			$zz['fields'][$no]['sql'] = sprintf('SELECT team_id
+					, CONCAT(team, IFNULL(CONCAT(" ", team_no),"")) AS team
+				FROM teams
+				WHERE event_id = %d
+				ORDER BY team_id', $brick['data']['event_id']);
+		} else {
+			$zz['fields'][$no]['hide_in_form'] = true;
+		}
+		break;
+	
+	case 'setzliste_no':
+	case 'qualification':
+	case 'qualification_event_id':
+	case 'urkundentext':
+		if (!empty($brick['data']['event_team'])) {
+			$zz['fields'][$no]['hide_in_form'] = true;
+		}
+		break;
+
+	case 'brett_no':
+	case 'rang_no':
+		if (!empty($brick['data']['event_single'])) {
+			$zz['fields'][$no]['hide_in_form'] = true;
+			$zz['fields'][$no]['hide_in_list'] = true;
+		}
+		break;
+
+	case 'gastspieler':
+		if (!$brick['data']['gastspieler'])
+			unset($zz['fields'][$no]);
+		break;
+
 	}
 }
 
-$zz['fields'][2]['display_field'] = 'person';
-if (brick_access_rights()) {
-	// @todo warum ist das write_once?
-	$zz['fields'][2]['type'] = 'select';
-}
+$zz['fields'][31]['title'] = 'Nation';
+$zz['fields'][31]['type'] = 'subtable';
+$zz['fields'][31]['table'] = 'participations';
+$zz['fields'][31]['table_name'] = 'participations_nation';
+$zz['fields'][31]['fields'] = [];
+$zz['fields'][31]['fields'][2]['type'] = 'foreign_key';
+$zz['fields'][31]['fields'][2]['field_name'] = 'participation_id';
+$zz['fields'][31]['class'] = 'block960';
+$zz['fields'][31]['hide_in_form'] = true;
+$zz['fields'][31]['hide_in_list_if_empty'] = true;
+$zz['fields'][31]['export_no_html'] = true;
+unset($zz['fields'][31]['subselect']['list_field_format']);
+$zz['fields'][31]['subselect']['sql'] = sprintf('SELECT participation_id
+		, federation
+	FROM participations
+	LEFT JOIN contacts_identifiers
+		ON contacts_identifiers.contact_id = participations.contact_id
+		AND contacts_identifiers.current = "yes"
+		AND contacts_identifiers.identifier_category_id = %d
+	LEFT JOIN fide_players
+		ON contacts_identifiers.identifier = fide_players.player_id
+', wrap_category_id('identifiers/id_fide'));
 
-$zz['fields'][5]['display_field'] = 'team';
-
-if ($brick['data']['turnierform'] !== 'e') {
-	$zz['fields'][14]['hide_in_form'] = true; // Setzliste
-	$zz['fields'][39]['hide_in_form'] = true; // Qualifkation
-	$zz['fields'][40]['hide_in_form'] = true; // Qualifikation über Termin
-	$zz['fields'][38]['hide_in_form'] = true; // Urkundentext
-
-	$zz['fields'][5]['sql'] = sprintf('SELECT team_id
-			, CONCAT(team, IFNULL(CONCAT(" ", team_no),"")) AS team
-		FROM teams
-		WHERE event_id = %d
-		ORDER BY team_id', $brick['data']['event_id']);
-
-} else {
-	$zz['fields'][50]['hide_in_form'] = true; // Brett
-	$zz['fields'][50]['hide_in_list'] = true; // Brett
-	$zz['fields'][13]['hide_in_form'] = true; // Gastspieler
-	$zz['fields'][10]['hide_in_form'] = true; // Rang
-	$zz['fields'][10]['hide_in_list'] = true; // Rang
-	$zz['fields'][5]['hide_in_form'] = true; // Team
-}
-
-if (brick_access_rights(['AK Spielbetrieb'])) {
-	$zz['fields'][31]['title'] = 'Nation';
-	$zz['fields'][31]['type'] = 'subtable';
-	$zz['fields'][31]['table'] = 'participations';
-	$zz['fields'][31]['table_name'] = 'participations_nation';
-	$zz['fields'][31]['fields'] = [];
-	$zz['fields'][31]['fields'][2]['type'] = 'foreign_key';
-	$zz['fields'][31]['fields'][2]['field_name'] = 'participation_id';
-	$zz['fields'][31]['class'] = 'block960';
-	$zz['fields'][31]['hide_in_form'] = true;
-	$zz['fields'][31]['hide_in_list_if_empty'] = true;
-	$zz['fields'][31]['export_no_html'] = true;
-	unset($zz['fields'][31]['subselect']['list_field_format']);
-	$zz['fields'][31]['subselect']['sql'] = sprintf('SELECT participation_id
-			, federation
-		FROM participations
-		LEFT JOIN contacts_identifiers
-			ON contacts_identifiers.contact_id = participations.contact_id
-			AND contacts_identifiers.current = "yes"
-			AND contacts_identifiers.identifier_category_id = %d
-		LEFT JOIN fide_players
-			ON contacts_identifiers.identifier = fide_players.player_id
-	', wrap_category_id('identifiers/id_fide'));
-}
 
 $zz['fields'][52]['field_name'] = 'date_of_birth';
 $zz['fields'][52]['type'] = 'display';
@@ -97,32 +110,20 @@ $zz['fields'][52]['title'] = 'Geb. Dat.';
 $zz['fields'][52]['search'] = 'persons.date_of_birth';
 $zz['fields'][52]['hide_in_form'] = true;
 
-$zz['fields'][53]['field_name'] = 'sex';
-$zz['fields'][53]['title_tab'] = 'S.';
-$zz['fields'][53]['type'] = 'display';
-$zz['fields'][53]['search'] = 'persons.sex';
-$zz['fields'][53]['hide_in_form'] = true;
-$zz['fields'][53]['character_set'] = 'latin1';
+$zz['fields'][66]['title'] = 'Verb.';
+$zz['fields'][66]['field_name'] = 'verband';
+$zz['fields'][66]['search'] = 'verbaende.contact_abbr';
+$zz['fields'][66]['character_set'] = 'utf8';
+$zz['fields'][66]['type'] = 'display';
+$zz['fields'][66]['hide_in_list_if_empty'] = true;
 
-if (brick_access_rights(['AK Spielbetrieb'])) {
-	$zz['fields'][66]['title'] = 'Verb.';
-	$zz['fields'][66]['field_name'] = 'verband';
-	$zz['fields'][66]['search'] = 'verbaende.contact_abbr';
-	$zz['fields'][66]['character_set'] = 'utf8';
-	$zz['fields'][66]['type'] = 'display';
-	$zz['fields'][66]['hide_in_list_if_empty'] = true;
+$zz['fields'][69]['title'] = 'FIDE-ID';
+$zz['fields'][69]['field_name'] = 'player_id_fide';
+$zz['fields'][69]['exclude_from_search'] = true;
+$zz['fields'][69]['type'] = 'display';
+$zz['fields'][69]['hide_in_list_if_empty'] = true;
+$zz['fields'][69]['hide_in_form'] = true;
 
-	$zz['fields'][69]['title'] = 'FIDE-ID';
-	$zz['fields'][69]['field_name'] = 'player_id_fide';
-	$zz['fields'][69]['exclude_from_search'] = true;
-	$zz['fields'][69]['type'] = 'display';
-	$zz['fields'][69]['hide_in_list_if_empty'] = true;
-	$zz['fields'][69]['hide_in_form'] = true;
-}
-
-if (!$brick['data']['gastspieler']) {
-	unset($zz['fields'][13]);
-}
 
 $zz['sql'] = 'SELECT participations.*
 		, IF(gastspieler = "ja", "ja", "") AS gastspieler_display
@@ -240,10 +241,7 @@ $zz['page']['extra']['wide'] = true;
 if (wrap_access('tournaments_merge'))
 	$zz['list']['merge'] = true;
 
-if (!brick_access_rights(['AK Spielbetrieb'])
-	AND !brick_access_rights(['Technik', 'Organisator', 'Turnierleiter'], $brick['data']['event_rights'])
-) {
+if (!wrap_access('tournaments_players_edit', $brick['data']['event_rights']))
 	$zz['access'] = 'none';
-}
 
 unset($zz['add']); // just add player here, no other groups
