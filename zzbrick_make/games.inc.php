83565886c3615396262d67094e99b06bbaf82db1
Gustaf Mossakowski <gustaf@koenige.org>
2025-07-20 11:24:06 +0200

    games: remove JOIN on categories, left over from 2024-05-18 when category was read from placeholder script (date)

--- zzbrick_make/games.inc.php
+++ zzbrick_make/games.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/tournaments
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2014-2024 Gustaf Mossakowski
+ * @copyright Copyright © 2014-2025 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
